736a3de50b4c7a6d43a01ef60b0cc4c57f75a903
JakirHossain <jakir.devbd@gmail.com>
2025-06-05 00:24:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    
+      <h1 className='text-4xl font-inter  font-extrabold text-red-500 text-center mt-10'>Allah is almighty.</h1>
+    
+    </>
+  )
+}
+
+export default App

f06b6435b3594e19c6f6844a4bbe7e98b5577ab6
JakirHossain <jakir.devbd@gmail.com>
2025-06-06 17:33:05 +0600

    Error page 404 added

---
+++ src/components/LottieAnimation.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import Lottie from "lottie-react";
+
+const LottieAnimation = ({ animationData, width = "100%", height = "100%", loop = true }) => {
+  return (
+    <div style={{ width, height }}>
+      <Lottie animationData={animationData} loop={loop} />
+    </div>
+  );
+};
+
+export default LottieAnimation;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import notFoundAnimation from "../assets/notFoundAnimation.json";
+import LottieAnimation from "../components/LottieAnimation";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="bg-gray-50 min-h-[450px] flex flex-col items-center justify-center px-4 py-12 my-10 text-center">
+      <LottieAnimation
+        animationData={notFoundAnimation}
+        width="800px"
+        height="500px"
+      />
+      {/* Illustration */}
 
-export default NotFound
+      {/* Button */}
+      <Link
+        to="/"
+        className="bg-indigo-500 text-white px-6 py-2 mt-6 rounded hover:bg-indigo-600 transition duration-300"
+      >
+        Go Back Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;

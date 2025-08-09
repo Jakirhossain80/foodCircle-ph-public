6f6fb4f67a25f192a34f17a820ec2c82ce875fe6
JakirHossain <jakir.devbd@gmail.com>
2025-08-10 02:20:51 +0600

    Some modification has been done in Navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,30 +1,20 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import { Outlet, useLocation } from "react-router";
+import { Outlet } from "react-router";
 import Home from "../components/Home";
 import ScrollToTop from "../utils/ScrollToTop";
-import getBreadcrumbItems from "../utils/getBreadcrumbItems";
-import Breadcrumb from "../components/Breadcrumb";
 
 const MainLayout = () => {
-   const location = useLocation();
-  const breadcrumbItems = getBreadcrumbItems(location.pathname);
   return (
     <>
+      <header>
+        <nav className="py-10 border-b border-gray-200 dark:border-gray-700">
+          <Navbar />
+        </nav>
+      </header>
       <div className="container">
-        <header>
-          <nav className="py-10">
-            <Navbar />
-          </nav>
-         <div className="mt-10">
-            <Breadcrumb items={breadcrumbItems}/>
-         </div>
-        
-        </header>
-          
         <main className="pt-10">
-         
           <Outlet>
             <Home />
           </Outlet>
